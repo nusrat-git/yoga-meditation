23890e79308feede344b0b3832faea6a97cfb5c7
nusrat-git <nusratjahanmithila66@gmail.com>
2022-09-29 18:16:52 +0600

    logo changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './Header.css';
-// import logo from '../../profile.jpg';
+import logo from '../../logo.png';
 
 const Header = () => {
     return (
-        <div className='nav'>
-            <nav>
-                <h1>Yoga Meditation</h1>
+        <div>
+            <nav className='nav'>
+                <div className='logo'>
+                    <img src={logo} alt="" />
+                </div>
+
+                <div>
+                    <a href="/home">Home</a>
+                    <a href="/about">About Us</a>
+                    <a href="/contact">Contact</a>
+                    <a href="/login">Login</a>
+                </div>
             </nav>
-            <div>
-                <a href="/home">Home</a>
-                <a href="/about">About Us</a>
-                <a href="/contact">Contact</a>
-                <a href="/login">Login</a>
-            </div>
         </div>
     );
 };
